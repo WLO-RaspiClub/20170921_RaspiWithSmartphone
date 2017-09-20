562a101ce435add2d7e52929795f3c4221e3ef2c
Takeshi Ueno <utaani@ueno.org>
2017-09-21 08:35:05 +0900

    version 2 (for blynk lib)

--- setup.sh
+++ setup.sh
@@ -2,3 +2,7 @@
 sudo apt-get install -y ipheth-utils libimobiledevice-utils ifuse usbmuxd
 sudo mkdir /media/iphone
 sudo echo 'user_allow_other' >> /etc/fuse.conf
+#
+sudo apt-get install build-essential
+sudo npm install -g onoff
+sudo npm install -g blynk-library
