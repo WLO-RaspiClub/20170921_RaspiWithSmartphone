ca185a1e394a87671f944364e1892c2f6412bb6a
Takeshi Ueno <utaani@ueno.org>
2017-09-21 08:40:45 +0900

    version3 (sudo file add fix)

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 sudo apt-get install -y ipheth-utils libimobiledevice-utils ifuse usbmuxd
 sudo mkdir /media/iphone
-sudo echo 'user_allow_other' >> /etc/fuse.conf
+echo 'user_allow_other' | sudo tee -a /etc/fuse.conf
 #
 sudo apt-get install build-essential
 sudo npm install -g onoff
